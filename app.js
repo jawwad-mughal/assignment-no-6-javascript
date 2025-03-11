523b777c1aa85506aaf05622ed25ec4f3e868760
unknown <jawad21ullah@gmail.com>
2025-03-11 22:55:21 +0500

    new

---
+++ app.js
@@ -0,0 +1,47 @@
+// STRING METHODS (start)
+
+// Q1
+// let firstName = prompt('Enter your first name');
+// let LastName = prompt('Enter your last name');
+// console.log(firstName.concat(' ',LastName))
+
+// Q2
+// let favoritePhone = prompt('Enter mobile model name');
+// let inputLength = favoritePhone ? favoritePhone.length: 0;
+// console.log('My favorite phone is:',favoritePhone,'length of string:',inputLength);
+
+// Q3
+// let country = prompt('enter country name');
+// console.log(country);
+// let letter = prompt('You entered "country". Enter a letter to find its index:');
+// let countryIndex = country.indexOf(letter);
+// console.log(countryIndex);
+
+// Q4
+// let input = prompt("message");
+// console.log(input);
+// let letters = prompt('You entered "message". Enter a letter to find its index:');
+// let message = input.lastIndexOf(letters);
+// console.log(message);
+
+// Q5
+// let country = prompt('Enter your country name');
+// console.log('String:',country);
+// let indexNumber = +prompt('search words single letter of index ');
+// let flag = false;
+// let message;
+
+// for(let i = 0; i < country.length; i++){
+//     if(i === indexNumber){
+//         let letterIndex = country.charAt(indexNumber);
+//         flag =true;
+//         message = `Character at index ${indexNumber}: ${letterIndex}`;
+//         break;
+//     }
+// }
+
+// if(!flag){
+//     message='This letter does not match. Please enter correct input.'
+// };
+// console.log(message);
+// STRING METHODS (end)
